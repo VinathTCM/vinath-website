7f6b43b35a3c5765ceea03f836f8a39bb426efb7
VinathTCM <315637148+VinathTCM@users.noreply.github.com>
2026-09-12 12:40:47 +0800

    chore: 移除临时脚本，加入.gitignore

--- _check_price.py
+++
@@ -1,8 +0,0 @@
-import io
-for f in ['shop/index.html','en/shop/index.html','product/index.html','en/product/index.html']:
-    s = io.open(f, encoding='utf-8').read()
-    print(f'=== {f} ===')
-    for kw in ['每包 RM', 'prod-price">MYR', 'feat-price">MYR', 'related-price">MYR', 'onwards', ' 起']:
-        cnt = s.count(kw)
-        if cnt:
-            print(f'  "{kw}" x{cnt}')
--- _fix_api_url.py
+++
@@ -1,7 +0,0 @@
-import io
-f = 'admin/orders/index.html'
-s = io.open(f, encoding='utf-8').read()
-s = s.replace("fetch(API_BASE + '/site-settings/shipping_settings')", "fetch('https://api.vinathtcm.com/api/site-settings/shipping_settings')")
-s = s.replace("fetch(API_BASE + '/admin/site-settings/shipping_settings'", "fetch('https://api.vinathtcm.com/api/admin/site-settings/shipping_settings'")
-io.open(f, 'w', encoding='utf-8').write(s)
-print('Done')
